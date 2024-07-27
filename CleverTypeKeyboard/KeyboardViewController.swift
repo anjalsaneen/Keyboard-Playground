6766ebe0ce24254cba62ef9629db953f71485750
anjalsaneen <43947967+anjalsaneen@users.noreply.github.com>
2024-07-27 16:19:12 +0530

    Added code to open app from keyboard. Not working

--- CleverTypeKeyboard/KeyboardViewController.swift
+++ CleverTypeKeyboard/KeyboardViewController.swift
@@ -1,30 +1,25 @@
-//
-//  KeyboardViewController.swift
-//  CleverTypeKeyboard
-//
-//  Created by Anjal Saneen on 12/07/24.
-//
-
 import UIKit
 import SwiftUI
 
 protocol KeyboardActionDelegate: AnyObject {
     func keyWasPressed(_ key: String)
+    func openHostApp(url: URL)
 }
 
-class KeyboardViewController: UIInputViewController {
+class KeyboardViewController: UIInputViewController, KeyboardActionDelegate {
     
     @IBOutlet var nextKeyboardButton: UIButton!
     
     override func updateViewConstraints() {
         super.updateViewConstraints()
-
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let customKeyboardView = CustomKeyboardView()
+        var customKeyboardView = CustomKeyboardView()
+        customKeyboardView.delegate = self // Set the delegate
+        
         let hostingController = UIHostingController(rootView: customKeyboardView)
         
         addChild(hostingController)
@@ -45,4 +40,46 @@ class KeyboardViewController: UIInputViewController {
         }
     }
     
+    func keyWasPressed(_ key: String) {
+    }
+    
+    func openHostApp(url: URL) {
+        extensionContext?.open(url, completionHandler: { (success) in
+            if success {
+                print("Opened \(url)")
+            } else {
+                print("Failed to open \(url)")
+            }
+        })
+    }
+}
+
+struct CustomKeyboardView: View {
+    weak var delegate: KeyboardActionDelegate?
+    var body: some View {
+        Spacer()
+        
+        Text("Open Host App")
+            .bold()
+            .multilineTextAlignment(.center)
+            .font(.system(size: 18))
+            .padding(.top, 12)
+        
+        Button(action: {
+            if let url = URL(string: "myapp://") {
+                print("clicked. opening url")
+                delegate?.openHostApp(url: url)
+            }
+        }) {
+            Text("Open App")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.vertical, 14)
+                .background(Color.blue)
+                .padding(.horizontal, 32)
+                .cornerRadius(10)
+        }.padding()
+        
+        Spacer()
+    }
 }
--- Playground/Others/PlaygroundApp.swift
+++ Playground/Others/PlaygroundApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct PlaygroundApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             MainView()
+        }
+    }
+    
+    class AppDelegate: NSObject, UIApplicationDelegate {
+        func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+            print("Received Url \(url)")
+            if url.scheme == "myapp" {
+                // Handle opening the app from the URL
+                print("App opened via URL scheme: \(url)")
+            }
+            return true
         }
     }
 }
